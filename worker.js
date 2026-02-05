55e4cc630debcf5f1e272112e08203acdaba99e6
Zr-udn <badboyav555@gmail.com>
2026-02-05 10:15:05 +0530

    Refactor fetch method to simplify request handling

--- worker.js
+++ worker.js
@@ -1,18 +1,11 @@
 export default {
   async fetch(request, env) {
 
-    if (request.method !== "POST") {
-      return new Response("Only POST allowed", { status: 405 });
-    }
+    const text = "Bhai alert aa gaya 🔔";
 
-    const data = await request.json();
+    const url = `https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage`;
 
-    const text = `🧪 New Form Data
-
-👤 Name: ${data.name}
-💬 Message: ${data.message}`;
-
-    await fetch(`https://api.telegram.org/bot${env.BOT_TOKEN}/sendMessage`, {
+    await fetch(url, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
